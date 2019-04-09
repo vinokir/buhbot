b39a9b18e85f74a177e77c537ca3aa2df4c006bc
PC\Кирилл <vinokir@mail.ru>
2019-04-09 19:33:57 +0700

    first commit

---
+++ Model/Updates.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuhBot.Model
+{
+    [Serializable]
+    public class Updates
+    {
+        /// <summary>
+        /// Тип события
+        /// </summary>
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Объект, инициировавший событие
+        /// Структура объекта зависит от типа уведомления
+        /// </summary>
+        [JsonProperty("object")]
+        public JObject Object { get; set; }
+
+        /// <summary>
+        /// ID сообщества, в котором произошло событие
+        /// </summary>
+        [JsonProperty("group_id")]
+        public long GroupId { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VkNet;
+using VkNet.Abstractions;
+using VkNet.Model;
+
+namespace BuhBot
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton<IVkApi>(sp => {
+                var api = new VkApi();
+                api.Authorize(new ApiAuthParams { AccessToken = Configuration["Config:AccessToken"] });
+                return api;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
