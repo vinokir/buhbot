485482b8a7a599166f0abd3de923a0c266febd85
PC\Кирилл <vinokir@mail.ru>
2019-04-09 21:27:51 +0700

    firstcommit

--- Controllers/CallbackController.cs
+++ Controllers/CallbackController.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BuhBot.Model;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using VkNet;
 using VkNet.Abstractions;
 using VkNet.Model;
 using VkNet.Model.RequestParams;
